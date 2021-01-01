fbfa8aa2042a8fa910ea632e242dc82c994b89d7
Qisthi Ramadhani <realramageek@gmail.com>
2021-01-01 11:07:04 +0700

    day 1

---
+++ src/pages/about.js
@@ -0,0 +1,34 @@
+import * as React from "react"
+
+// styles
+const pageStyles = {
+    color: "#232129",
+    padding: "96px",
+    fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+
+const headingStyles = {
+    marginTop: 0,
+    marginBottom: 64,
+    maxWidth: 320,
+}
+const headingAccentStyles = {
+  color: "#663399",
+}
+
+const AboutPage = () => {
+    return (
+        <main style={pageStyles}>
+            <h1 style={headingStyles}>
+                It's about.js file
+                <br />
+                <span style={headingAccentStyles}>— I just made a page in Gatsby site! </span>
+                <span role="img" aria-label="Party popper emojis">
+                    🎉🎉
+                </span>
+            </h1>
+        </main>
+    )
+}
+
+export default AboutPage
---
+++ src/pages/location/san-jose.js
@@ -0,0 +1,34 @@
+import * as React from "react"
+
+// styles
+const pageStyles = {
+    color: "#232129",
+    padding: "96px",
+    fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+
+const headingStyles = {
+    marginTop: 0,
+    marginBottom: 64,
+    maxWidth: 320,
+}
+const headingAccentStyles = {
+  color: "#663399",
+}
+
+const SanJosePage = () => {
+    return (
+        <main style={pageStyles}>
+            <h1 style={headingStyles}>
+                It's location/san-jose.js file
+                <br />
+                <span style={headingAccentStyles}>— I just made a page in Gatsby site! </span>
+                <span role="img" aria-label="Party popper emojis">
+                    🎉🎉
+                </span>
+            </h1>
+        </main>
+    )
+}
+
+export default SanJosePage
